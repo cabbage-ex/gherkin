3575910234259e8d36f23f2c931609dfadc00bcd
Regnis Olbap <singer.pablo@gmail.com>
2019-07-09 11:13:18 +0200

    Add get_line_name method to Line module

--- lib/gherkin/elements/line.ex
+++ lib/gherkin/elements/line.ex
@@ -1,5 +1,10 @@
 defmodule Gherkin.Elements.Line do
   @moduledoc false
-  
+
   defstruct raw_text: "", text: "", line_number: nil
+
+  def get_line_name(line_text) do
+    [_, line_name] = String.split(line_text, ":", parts: 2)
+    line_name
+  end
 end
